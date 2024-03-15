1c8652c9abf700d13c330dde23f430ff54c98f69
ɧąƈƙɛཞ <154057000+75atulkumar@users.noreply.github.com>
2024-03-15 13:10:48 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28912814
 api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6364657966:AAFurmgKzKBNDaFDTVBnya1eZYMWOs9ua2o"
+bot_token = "7016936062:AAHPWSTnTG767TXUCi65utdJXF15HcOJji8"
 AUTH = 7140557396
-#Bot Username = "hackerhahshevbot"
+#Bot Username = "annukijaanbot"
