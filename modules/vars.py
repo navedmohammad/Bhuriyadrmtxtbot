93d5d1ee7f745e3f27391a75e514a6547d261b45
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-08-10 13:21:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
-api_id = 26451206
-api_hash = "32984406271d6f3945bb536671b143a7"
-bot_token = "7010963603:AAEaabkjr2DjiX2vhD8gVbkVlKWMrJTByPc"
-AUTH = 6830450483
+api_id = 22475741
+api_hash = "1a217be71a0225e0a678af286c211f8a"
+bot_token = "7086426763:AAFAbz6xw3u0F7EfQnpeZ0nmeOloLkpvKC0"
+AUTH = 6562642609
 #Bot Username = "bhuriyabhaig"
